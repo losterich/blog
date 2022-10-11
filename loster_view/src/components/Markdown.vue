7d1d83738b35def0d3fefdfdac13417eb21f7715
losterich <1307942769@qq.com>
2022-10-11 17:48:47 +0800

    markdown文本渲染组件

---
+++ loster_view/src/components/Markdown.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <div class="title-box">
+      <h1>{{essayData.title}}</h1>
+      <p>@{{essayData.author}}，{{essayData.date}}</p>
+    </div>
+  <div v-highlight class="text markdown-body" v-html="html"/>
+  </div>
+</template>
+
+<script>
+export default {
+    props:['html', 'essayData'],
+}
+</script>
+
+<style lang="less" scoped>  
+// github markdown样式
+@import 'github-markdown-css/github-markdown.css';
+
+.title-box {
+  line-height: 40px;
+  margin-bottom: 50px;
+}
+.text{
+  line-height:25px;
+  text-align:left;
+  text-align: left;
+}
+
+</style>
